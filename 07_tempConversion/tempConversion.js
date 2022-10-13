60b303f8519c90cf4ea6a943684b13b61c256312
Justin Sears <justinrsears@gmail.com>
2022-10-12 20:36:35 -0700

    Finish exercises 06 and 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celTemp = (temp - 32) * (5 / 9);
+  let celTempRound = Math.round(celTemp * 10) / 10;
+  return celTempRound;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahTemp = (temp * (9 / 5) + 32);
+  let fahTempRound = Math.round(fahTemp * 10) / 10;
+  return fahTempRound;
 };
+
+console.log(ftoc(100));
+console.log(ctof(73.2));
 
 // Do not edit below this line
 module.exports = {
