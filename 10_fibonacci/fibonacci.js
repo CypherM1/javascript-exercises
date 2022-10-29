648a0a3311ddbd1b87e99eee1d90dbddc2823224
Justin Sears <justinrsears@gmail.com>
2022-10-29 16:54:23 -0700

    Finished 10_fibonacci javascript-exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(seqNum) {
+    if (Number(seqNum) < 0){
+        return ("OOPS");
+    } else if (Number(seqNum) == 1) {
+        return (1);    
+    } else {
+        a = 0;
+        b = 1;
+        for (let i = 0; i < Number(seqNum) - 1 ; i++) {
+            answer = a + b
+            a = b;
+            b = answer;
+        }
+        return(answer);
+    };
 };
+
+fibonacci("3");
 
 // Do not edit below this line
 module.exports = fibonacci;
