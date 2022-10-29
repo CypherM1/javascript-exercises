b2e2d7a404d1fd45761e6a22cf1c72d085d63974
Justin Sears <justinrsears@gmail.com>
2022-10-28 23:05:40 -0700

    Finished 09_palindromes javascript-exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (sentance) {
+    let sentanceNoSpaceNoPunctuation = sentance.replace(/[\s+.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    let sentanceAllLowerCase = sentanceNoSpaceNoPunctuation.toLowerCase();
+    let sentanceReverse = '';
+    for (var i = sentanceAllLowerCase.length - 1; i >= 0; i--) {
+        sentanceReverse += sentanceAllLowerCase[i];
+    }
+    if (sentanceReverse === sentanceAllLowerCase) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
